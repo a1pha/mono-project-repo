5589d8d111276001e84f336339693371ae247705
Abhi Jadhav <abhijadhav9571@gmail.com>
2025-07-12 18:02:55 -0700

    add skeleton

--- pointer-wars/level1/linked_list.c
+++ pointer-wars/level1/linked_list.c
@@ -6,5 +6,151 @@
 static void * (*malloc_fptr)(size_t size) = NULL;
 static void   (*free_fptr)(void* addr)    = NULL; 
 
-(void)(malloc_fptr);
-(void)(free_fptr);
+// Creates a new linked_list.
+// PRECONDITION: Register malloc() and free() functions via the
+//               linked_list_register_malloc() and 
+//               linked_list_register_free() functions.
+// POSTCONDITION: An empty linked_list has its head point to NULL.
+// Returns a new linked_list on success, NULL on failure.
+//
+struct linked_list * linked_list_create(void) {
+    (void)malloc_fptr;
+    (void)free_fptr;
+    return NULL;
+}
+
+// Deletes a linked_list and frees all memory assoicated with it.
+// \param ll : Pointer to linked_list to delete
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_delete(struct linked_list * ll) {
+    (void)ll;
+    (void)free_fptr;
+    return false;
+}
+
+// Returns the size of a linked_list.
+// \param ll : Pointer to linked_list.
+// Returns size on success, SIZE_MAX on failure.
+//
+size_t linked_list_size(struct linked_list * ll) {
+    (void)ll;
+    return SIZE_MAX;
+}
+
+// Inserts an element at the end of the linked_list.
+// \param ll   : Pointer to linked_list.
+// \param data : Data to insert.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_insert_end(struct linked_list * ll,
+                            unsigned int data) {
+    (void)ll;
+    (void)data;
+    (void)malloc_fptr;
+    return false;
+}
+
+// Inserts an element at the front of the linked_list.
+// \param ll   : Pointer to linked_list.
+// \param data : Data to insert.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_insert_front(struct linked_list * ll,
+                              unsigned int data) {
+    (void)ll;
+    (void)data;
+    (void)malloc_fptr;
+    return false;
+}
+
+// Inserts an element at a specified index in the linked_list.
+// \param ll    : Pointer to linked_list.
+// \param index : Index to insert data at.
+// \param data  : Data to insert.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_insert(struct linked_list * ll,
+                        size_t index,
+                        unsigned int data) {
+    (void)ll;
+    (void)index;
+    (void)data;
+    (void)malloc_fptr;
+    return false;
+}
+
+// Finds the first occurrence of data and returns its index.
+// \param ll   : Pointer to linked_list.
+// \param data : Data to find.
+// Returns index of the first index with that data, SIZE_MAX otherwise.
+//
+size_t linked_list_find(struct linked_list * ll,
+                        unsigned int data) {
+    (void)ll;
+    (void)data;
+    return SIZE_MAX;
+}
+
+// Removes a node from the linked_list at a specific index.
+// \param ll    : Pointer to linked_list.
+// \param index : Index to remove node.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_remove(struct linked_list * ll,
+                        size_t index) {
+    (void)ll;
+    (void)index;
+    (void)free_fptr;
+    return false;
+}
+
+// Creates an iterator struct at a particular index.
+// \param linked_list : Pointer to linked_list.
+// \param index       : Index of the linked list to start at.
+// Returns pointer to an iterator on success, NULL otherwise.
+//
+struct iterator * linked_list_create_iterator(struct linked_list * ll,
+                                              size_t index) {
+    (void)ll;
+    (void)index;
+    (void)malloc_fptr;
+    return NULL;
+}
+
+// Deletes an iterator struct.
+// \param iterator : Iterator to delete.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_delete_iterator(struct iterator * iter) {
+    (void)iter;
+    (void)free_fptr;
+    return false;
+}
+
+// Iterates to the next node in the linked_list.
+// \param iterator: Iterator to iterate on.
+// Returns TRUE when next node is present, FALSE once end of list is reached.
+//
+bool linked_list_iterate(struct iterator * iter) {
+    (void)iter;
+    return false;
+}
+
+// Registers malloc() function.
+// \param malloc : Function pointer to malloc()-like function.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_register_malloc(void * (*malloc)(size_t)) {
+    (void)malloc;
+    return false;
+}
+
+// Registers free() function.
+// \param free : Function pointer to free()-like function.
+// Returns TRUE on success, FALSE otherwise.
+//
+bool linked_list_register_free(void (*free)(void*)) {
+    (void)free;
+    return false;
+}
